93f6c2c54f919c95ff7b1bb93d691c478e6e0fb1
Brandon Bloom <brandon@brandonbloom.name>
2013-01-02 16:03:00 -0800

    Update :url and :description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject factjor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Factor-inspired, concatenative DSL for Clojure"
+  :url "https://github.com/brandonbloom/factjor"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
